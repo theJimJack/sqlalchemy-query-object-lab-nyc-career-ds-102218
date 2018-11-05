531901dbdd4a141f483e51a2303aa50f88ee1340
Jim Jack <jim_jack@Jims-MacBook-Pro.local>
2018-11-05 17:07:04 -0500

    updated from 2018-11-05

--- queries.py
+++ queries.py
@@ -7,17 +7,23 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def return_apple():
-    return session.query(Company).filter(Company.symbol=="AAPL")
+    return session.query(Company).filter(Company.company=="Apple").first()
 
 def return_disneys_industry():
-    pass
+    disney = session.query(Company).filter(Company.company=="Walt Disney").first()
+    return disney.industry
 
 def return_list_of_company_objects_ordered_alphabetically_by_symbol():
-    pass
+    return session.query(Company).order_by(Company.symbol).all()
 
 def return_list_of_dicts_of_tech_company_names_and_their_EVs_ordered_by_EV_descending():
-    pass
-
+    tech = session.query(Company).filter_by(industry="Technology").order_by(Company.enterprise_value.desc()))
+    list = []
+    for c in tech:
+        obj = {'company':c.company,'EV':c.enterprise_value}
+        list.append(obj)
+    return list
+    
 def return_list_of_consumer_products_companies_with_EV_above_225():
     pass
 
